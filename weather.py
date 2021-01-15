a581cdeaa3e03725689c1bf0679409fe544c6f03
Yann <yann.bezier@viacesi.fr>
2021-01-15 09:07:35 +0100

    Gestion météorologique
    Récupération et affichage de la meteo du lieu voulu

--- weather.py
+++ weather.py
@@ -5,7 +5,9 @@ city = input("City Name :")
 url = api_address + city
 
 json_data = requests.get(url) .json()
-formatted_data = json_data['weather'] [0] ['description'] # Nous permet d'affiché la description lors de l'execution du script
-print(formatted_data)
+cloud = json_data['weather'] [0] ['description'] # Nous permet d'affiché la description lors de l'execution du script
+humidity = json_data['main'] ['humidity']
+print("Actuellement le temps est : " + cloud)
+print("Et le taux d'humidité est de : ", end="") ; print(humidity)
 
 # si vous voulez essayer le script : executer le et taper le nom de la ville qui vous interesse :) . *Yann
