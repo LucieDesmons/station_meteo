0cd8db62663a967e067df0d92c750ee7240dbf8a
Yann <yann.bezier@viacesi.fr>
2021-01-15 09:26:09 +0100

    Weather
    Ajout commentaire

--- weather.py
+++ weather.py
@@ -1,13 +1,13 @@
 import requests
 
-api_address = 'http://api.openweathermap.org/data/2.5/weather?APPID=aa5031fee8175b3801b195a4a885f30e&lang=fr&q='
-city = input("City Name :")
-url = api_address + city
+api_address = 'http://api.openweathermap.org/data/2.5/weather?APPID=aa5031fee8175b3801b195a4a885f30e&lang=fr&q=' # on recupere l'adresse de l'API en ayant pris soin de ne pas selectionné de ville dans l'URL 'q='
+city = input("City Name :") # on fait un prompt pour récupérer la ville souhaité par le client 
+url = api_address + city                            # concaténation URL + Ville
 
-json_data = requests.get(url) .json()
-cloud = json_data['weather'] [0] ['description'] # Nous permet d'affiché la description lors de l'execution du script
-humidity = json_data['main'] ['humidity']
+json_data = requests.get(url) .json()               #
+cloud = json_data['weather'] [0] ['description']    # Nous permet d'affiché la description lors de l'execution du script dans un print()
+humidity = json_data['main'] ['humidity']           # ainsi que l'humidité dans un print()
 print("Actuellement le temps est : " + cloud)
-print("Et le taux d'humidité est de : ", end="") ; print(humidity)
+print("Et le taux d'humidité est de : ", end="") ; print(humidity, end="%") # le end permet de ne pas aller a la ligne &ou ajouter un print()
 
 # si vous voulez essayer le script : executer le et taper le nom de la ville qui vous interesse :) . *Yann
